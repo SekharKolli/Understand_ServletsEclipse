cb142a3036a5b26a1ac80d4c65184f852a475315
Sekhar Kolli <Sekhar.Kolli@gmail.com>
2019-02-02 21:35:52 -0500

    Function to read an existing HTML page using JavaStreams

--- src/sk/learns/formCreation/ShowSimpleLogin.java
+++ src/sk/learns/formCreation/ShowSimpleLogin.java
@@ -33,8 +33,7 @@ public class ShowSimpleLogin extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 
-		myHTMLPage(out);
-//		readHTMLFile(out);
+		readHTMLFile(out);
 
 	}// EO doGet()
 	
@@ -47,39 +46,4 @@ public class ShowSimpleLogin extends HttpServlet {
 		
 	}// EO methodName()
 
-	private void myHTMLPage(PrintWriter out) {                                                                                 
-                                                                                                                               
-		out.println("<!DOCTYPE html>                                                                                           ");
-		out.println("<html>                                                                                                    ");
-		out.println("<head>                                                                                                    ");
-		out.println("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">                                ");
-		out.println("  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">");
-		out.println("  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>              ");
-		out.println("  <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\"></script>           ");
-		out.println("</head>                                                                                                   ");
-		out.println("<body>                                                                                                    ");
-        out.println("                                                                                                          ");
-		out.println("<div class=\"container\">                                                                                 ");
-		out.println("  <h2>Basic Login Page...</h2>                                                                            ");
-		out.println("  <form>                                                                                                  ");
-		out.println("    <div class=\"form-group\">                                                                            ");
-		out.println("      <label for=\"email\">Email:</label>                                                                 ");
-		out.println("      <input type=\"email\" class=\"form-control\" id=\"email\" placeholder=\"Enter email\">              ");
-		out.println("    </div>                                                                                                ");
-		out.println("    <div class=\"form-group\">                                                                            ");
-		out.println("      <label for=\"pwd\">Password:</label>                                                                ");
-		out.println("      <input type=\"password\" class=\"form-control\" id=\"pwd\" placeholder=\"Enter password\">          ");
-		out.println("    </div>                                                                                                ");
-		out.println("    <div class=\"checkbox\">                                                                              ");
-		out.println("      <label><input type=\"checkbox\"> Remember me</label>                                                ");
-		out.println("    </div>                                                                                                ");
-		out.println("    <button type=\"submit\" class=\"btn btn-default\">Submit</button>                                     ");
-		out.println("  </form>                                                                                                 ");
-		out.println("</div>                                                                                                    ");
-        out.println("                                                                                                          ");
-		out.println("</body>                                                                                                   ");
-		out.println("</html>																								   ");	
-		
-		
-	}// EO generateSendHTML()
 }// EOC ShowSimpleLogin
